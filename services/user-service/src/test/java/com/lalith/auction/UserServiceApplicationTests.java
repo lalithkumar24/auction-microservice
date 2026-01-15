662478407a2740605303ff189e5cb9f1b4801317
lalith kumar <lalithkumar2004@outlook.com>
2026-01-15 18:05:13 +0530

    Added newUser User-service

--- services/user-service/src/test/java/com/lalith/auction/UserServiceApplicationTests.java
+++ services/user-service/src/test/java/com/lalith/auction/UserServiceApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuctionServiceApplicationTests {
+class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
